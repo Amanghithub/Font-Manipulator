09d2257501d9b9f465eb82003f4a59bb0d0f6153
Amanghithub <94109038+Amanghithub@users.noreply.github.com>
2022-11-01 17:42:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup(){
+    canvas=createCanvas(550,500);
+    canvas.position(560,150);
+
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    
+}
+
+function draw(){
+background("white");
+
+}
